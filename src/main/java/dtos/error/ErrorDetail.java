0a5811035551117d1c9e3533f5b35bd63607cc0d
Jeremy <jeremysabina@gmail.com>
2021-08-18 10:44:41 -0400

    Fine LEON

---
+++ src/main/java/dtos/error/ErrorDetail.java
@@ -0,0 +1,49 @@
+package dtos.error;
+
+public class ErrorDetail {
+    String title;
+    int status = 0;
+    String detail;
+    long timeStamp = 0;
+    String developerMessage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+}
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/exception/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package io.zipcoder.tc_spring_poll_application.exception;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+
+}
